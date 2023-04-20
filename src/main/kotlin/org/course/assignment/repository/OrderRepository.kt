2b3bf9cdda9a2f170c976d628c285f880d1bb279
prabirparida <prabir.parida@ah.nl>
2023-04-20 11:50:06 +0200

    commit

--- src/main/kotlin/org/course/assignment/repository/OrderRepository.kt
+++ src/main/kotlin/org/course/assignment/repository/OrderRepository.kt
@@ -2,18 +2,18 @@ package org.course.assignment.repository
 
 import org.course.assignment.model.Bill
 import org.course.assignment.model.Item
-import org.springframework.stereotype.Component
+import org.course.assignment.service.ProductService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 import java.util.*
 
-@Component
+
 @Repository
-class OrderRepository(
-    val productRepository: ProductRepository
+class OrderRepository @Autowired constructor(
+    val productRepository: ProductRepository,
+    val productService: ProductService
 ) {
-    companion object{
-        lateinit var bill: Bill
-    }
+
 
     fun getCalculateBill(items: List<Item>): Bill? {
         val productIds = items.map { it.id }
@@ -21,14 +21,16 @@ class OrderRepository(
         val finalId = products.map { it.id }
         val sortItem = items.filter { it.id in finalId }.sortedBy { it.id }
         val sortProduct = products.filter { it.id in finalId }.sortedBy { it.id }
+        var bill: Bill = Bill()
         bill.totalItems = 0
         bill.totalPrice = 0.0
-        for (i in 0..finalId.size){
-            if(sortItem[i].quantity < sortProduct[i].quantity){
+        for (i in 0 until finalId.size - 1) {
+            if (sortItem[i].quantity < sortProduct[i].quantity) {
                 bill.totalItems += sortItem[i].quantity
                 bill.totalPrice += (sortItem[i].quantity * sortProduct[i].price)
             }
         }
         return bill
+// return null
     }
 }
